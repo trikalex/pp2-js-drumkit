70ff9b7705cae70d5667732eb0fa35bc4a1314c4
Alexandru Ursu <bodhipankh@yahoo.com>
2023-01-04 05:42:08 +0000

    Add more js structure and debug

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -1,16 +1,25 @@
 // let kits = document.getElementById('kitsel');
-let drumsEl = document.querySelector('.drums');
-let audioEl = document.createElement('audio');
-let btnEl = document.getElementsByClassName('btn');
-    drumsEl.appendChild(audioEl);
 
-let kitSelect = document.querySelectorAll('select[id$="kitsel"]');
+// let drumsEl = document.querySelector('.drums');
+let btnEl = document.getElementsByClassName('btn');
+
+
+
+function addAudioToButtons() {
+    let audioEl = document.createElement('audio');
+    document.getElementsByClassName('drums').appendChild(audioEl);
+    // btnEl.appendChild(audioEl);
+}
+    
+
+let kitSelect = document.querySelectorAll('select[id="kitsel"]');
 kitSelect.forEach(select => {
     select.addEventListener('change', changeDrumKit);
 });
 
 function changeDrumKit(e) {
-    let opt = e.target.selectedOptions[0].value;
+    
+    let opt = e.target.selectedOptions.value;
 
     if (opt.includes('kit1')) {
         audioEl.src = 'assets/audio/kit1/' + kit + '1.wav';
@@ -28,15 +37,17 @@ function changeDrumKit(e) {
     btnEl.addEventListener('click', ()=>{
         audioEl.play();
     });
-    // window.addEventListener('keydown', (event)=>{
-    //     if (event.key === ) {
-    //         audioEl.play();
-    //         btnEl.style.transform ='scale(.9)';
-    //         setTimeout(()=>{btnEl.style.transform = 'scale(1)';},);
-    //     };
-    // });
+
+
+//     // window.addEventListener('keydown', (event)=>{
+//     //     if (event.key === ) {
+//     //         audioEl.play();
+//     //         btnEl.style.transform ='scale(.9)';
+//     //         setTimeout(()=>{btnEl.style.transform = 'scale(1)';},);
+//     //     };
+//     // });
+
 };
 
-// console.log(opt);
-console.log(audioEl);
-console.log(drumsEl);
+// console.log(drumsEl);
+// console.log(audioEl);
